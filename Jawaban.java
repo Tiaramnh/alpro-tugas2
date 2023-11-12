5043f913796b45a583293a127119ca7387e9f76f
Tiara332 <148456031+Tiaramnh@users.noreply.github.com>
2023-11-12 21:22:55 +0700

    Tipe Data Pada Java

---
+++ Jawaban.java
@@ -0,0 +1,22 @@
+/*Tipe data dibagi menjadi 2 kategori yaitu :
+a.Tipe data primitif
+  Tipe data yang di persiapkan untuk pemrograman konvensional atau prosedural. Berikut adalah tipe data primitif yaitu:
+  - bilangan bulat (integer) : integer merupaka tipe data numerik untuk bilangan bulat dan berbentuk bilangan negatif maupun positif.
+  - bilangan pecahan (floating point) : tipe data ini merupakan tipe data yang menangani bilangan desimal atau pecahan dan digunakan untuk perhitungan secara detail.
+  - karakter (char) : tipe data ini merupakan tipe data yang biasa didefinisikan menggunakan tanda petik (') si awal dan akhir karakter.
+  - logika (boolean) : tipe data ini merupakan tipe data yang biasa didefinisikan untuk tipe yang memiliki nilai benar (true) atau salah (false).
+b. Tipe Data Referensi 
+    tipe data ini merupakan tipe data objek dari kelas tertentu dan sering digunakan dalam aplikasi yang sudah berbasis Object Oriented Programming (OOP),tipe data ini sering di sebut juga tipe data non-primitif atau juga composite. berikut beberapa tipe data referensi :
+   - String : Tipe data yang digunakan untk menyimpan urutan karakter (teks) dan nilai dari string yang harus diapit oleh tanda kutip ganda ("").
+   - Array : merupakan tipe data yang berisi sekumpulan nilai yang memiliki tipe data yang sama.
+Berikut contoh tipe data : */
+
+public class Boolean {
+public static void main(String[] args) {
+
+  boolean Apakahwanitacantik = true;
+  boolean Apakahpriacantik = false;
+  System.out.println(Apakahwanitacantik);
+  System.out.println(Apakahpriacantik);
+}
+}
