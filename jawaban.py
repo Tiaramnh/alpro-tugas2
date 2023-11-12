32b00c03d4d6e20503f9fc39eaf9be1a916bfdd7
Tiara332 <148456031+Tiaramnh@users.noreply.github.com>
2023-11-12 21:23:25 +0700

    Tipe Data Pada Python

---
+++ jawaban.py
@@ -0,0 +1,15 @@
+# berikut adalah tipe data yang ada pada python
+# Tipe Numerik - Integer : tipe iini adalah tipe data pada python yang berkaitan dengan angka.contoh:
+bilangan_bulat = 30 
+# Tipe Nurmerik - Float :tipe data ini adalah salh satu bagian dari tipe data numerik. tipe data float akan berkaitan erat dengan data numerik yang berbentuk pecahan atau desimal.contoh :
+    x = int(3.14)
+    print(x)
+# Tipe data String : tipe data ini merupakan tipe data yang berkaitan erat dengan data yang bentuknya teks. dala proses analisis data, data yang kita butuhkan terkadang tidak hanya data numerik, namun juga data string.contoh :
+    sapa = "Hello"
+    nama = "Mutiara"
+    kalimat = sapa + " " + nama
+    print(kalimat)
+# Tipe Data Boolean : tipe data ini adlah tipe data yang digunkan untuk mempersentasikan nilai kebenaran. data in akan erat kaitannya dengan logika matematika, dimana datanya hanya akan terdiri dari dua jenis, yaitu TRUE dan FALSE. contoh :
+    x = 3
+    y = 5
+    print(x > y)
